dfd4c7cb0c7acbfb7b9640c0aec72a35c1c0917f
Fabian Valencia <fabiansvc@github.com>
2022-12-07 16:17:47 -0500

    webpack implemented

---
+++ bundler/webpack.common.js
@@ -0,0 +1,123 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
+
+module.exports = {
+    entry: path.resolve(__dirname, '../src/index.js'),
+    output:
+    {
+        hashFunction: 'xxhash64',
+        path: path.resolve(__dirname, '../dist'),
+        filename: '[name].[contenthash].js',
+        publicPath: ""
+    },
+    devtool: 'source-map',
+    plugins:
+        [
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: path.resolve(__dirname, '../static') }
+                ]
+            }),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, '../public/index.html'),
+                minify: true
+            }),
+            new MiniCSSExtractPlugin()
+        ],
+    module:
+    {
+        rules:
+            [
+                {
+                    test: /\.(scss)$/,
+                    use: [
+                        {
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader'
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                postcssOptions: {
+                                    plugins: () => [
+                                        require('autoprefixer')
+                                    ]
+                                }
+                            }
+                        },
+                        {
+                            loader: 'sass-loader'
+                        }
+                    ]
+                },
+                // HTML
+                {
+                    test: /\.(html)$/,
+                    use:
+                        [
+                            'html-loader'
+                        ]
+                },
+
+                // JS
+                {
+                    use: "babel-loader",
+                    test: /.(js|jsx)$/,
+                    exclude: /node_modules/
+                },
+
+                // CSS
+                {
+                    test: /\.css$/,
+                    use:
+                        [
+                            MiniCSSExtractPlugin.loader,
+                            'css-loader'
+                        ]
+                },
+
+                // Images
+                {
+                    test: /\.(jpg|png|gif|svg)$/,
+                    type: 'asset/resource',
+                    generator:
+                    {
+                        filename: 'assets/images/[hash][ext]'
+                    }
+                },
+
+                // Fonts
+                {
+                    test: /\.(ttf|eot|woff|woff2)$/,
+                    type: 'asset/resource',
+                    generator:
+                    {
+                        filename: 'assets/fonts/[hash][ext]'
+                    }
+                },
+
+                // Shaders
+                {
+                    test: /\.(glsl|vs|fs|vert|frag)$/,
+                    type: 'asset/source',
+                    generator:
+                    {
+                        filename: 'assets/images/[hash][ext]'
+                    }
+                }
+            ]
+    },
+    // PATH RESOLVE
+    resolve: {
+        extensions: ['.js', '.json', '.jsx'],
+
+        modules: [
+        path.resolve(__dirname, 'src'),
+            'node_modules'
+        ]
+    },
+}
--- src/Experience.js
+++ src/Experience.js
@@ -3,7 +3,7 @@ import { Center, useGLTF, OrbitControls } from '@react-three/drei'
 export default function Experience()
 {
     const { nodes, materials } = useGLTF('./model/domo.glb')
-    console.log(nodes);
+
     return <>
         <ambientLight intensity={1.0}></ambientLight>
         <directionalLight color="white" intensity={2.0} position={[2, 1, 2]} />
@@ -15,12 +15,11 @@ export default function Experience()
             enableZoom = {false} 
             minPolarAngle = {Math.PI * 0.5 -0.5}
             maxPolarAngle = {Math.PI }
-            
         />
         <Center>
             <mesh 
-            geometry={ nodes.sphere.geometry }
-            material={materials.gray}>
+                geometry={ nodes.sphere.geometry }
+                material={materials.gray}>
             </mesh>
         </Center>       
     </>
