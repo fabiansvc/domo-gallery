d11fc9e67f713a091a2a372ccee77d44b3dfe2ed
Fabian Valencia <fabiansvc@github.com>
2023-01-23 01:02:13 -0500

    domo updgrade

--- src/Experience.js
+++ src/Experience.js
@@ -13,30 +13,48 @@ export default function Experience() {
         return vid;
     });
 
-    const { nodes, materials } = useGLTF('./model/domo.glb')
-
+    const { nodes, materials } = useGLTF('./model/domo/domo.glb')
+    console.log(materials);
     return <>
         <ambientLight intensity={0.5}></ambientLight>
         <directionalLight color="white" intensity={2.0} position={[2, 1, 2]} />
-        
+
         <OrbitControls
             enableDamping={true}
             minDistance={0}
             maxDistance={0.5}
             enablePan={false}
             enableZoom={true}
-            // minPolarAngle={Math.PI * 0.5 - 0.5}
-            // maxPolarAngle={Math.PI}
+            minPolarAngle={Math.PI * 0.5 - 0.5}
+            maxPolarAngle={Math.PI}
         />
         <Center>
             <mesh
-                geometry={nodes.sphere.geometry}
-                material={materials.gray}>
+                geometry={nodes.Floor.geometry}
+                material={materials.MetalGray}
+            >
             </mesh>
-            <mesh geometry={nodes.panel.geometry}>
+            <mesh
+                geometry={nodes.Top.geometry}
+                material={materials.MetalGray}
+            >
+            </mesh>
+            <mesh
+                geometry={nodes.BottomStructure.geometry}
+                material={materials.MetalRed}
+            >
+            </mesh>
+            <mesh
+                geometry={nodes.Screen.geometry}
+            >
                 <meshBasicMaterial>
                     <videoTexture flipY={false} attach="map" args={[video]} />
                 </meshBasicMaterial>
+            </mesh>
+            <mesh
+                geometry={nodes.TopStructure.geometry}
+                material={materials.MetalRed}
+            >
             </mesh>
         </Center>
     </>
