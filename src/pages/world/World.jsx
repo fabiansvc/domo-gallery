1cbbe50c3daa6511975dcd4abf5d223a911f5d30
Fabian Stiven Valencia Cordoba <fabian.cordoba@correounivalle.edu.co>
2023-09-10 00:19:25 -0500

    Domo optimized

--- src/pages/world/World.jsx
+++ src/pages/world/World.jsx
@@ -3,7 +3,6 @@ import Avatar from "./components/Avatar";
 import Domo from "./components/Domo";
 import Environments from "./components/Environments";
 import Gallery from "./components/Gallery";
-import Info from "./components/Info";
 import PhysicsBodies from "./components/PhysicsBodies";
 import Player from "./components/Player";
 import Cursor from "./components/Cursor";
@@ -11,7 +10,6 @@ import keyboardControls from "../../utils/keyBoardControls";
 import { Center, KeyboardControls, PointerLockControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import { ACESFilmicToneMapping } from "three";
 import Logout from "../../components/Logout/Logout";
 import Instructive from "../../components/Instructive/Instructive";
 
@@ -24,17 +22,13 @@ const World = () => {
                 map={keyboardControls}>
                 <Canvas
                     className='canvas'
-                    dpr={[1,2]}
-                    flat
                     gl={
                         {
-                            antialias: true,
-                            toneMapping: ACESFilmicToneMapping
-
+                            antialias: true
                         }
                     }
                     camera={{
-                        fov: 50,
+                        fov: 60,
                         near: 0.1,
                         far: 40,
                         position: [14.25, -2.5, -5],
@@ -48,7 +42,6 @@ const World = () => {
                             <Center>
                                 <PhysicsBodies />
                                 <Gallery />
-                                <Info position={[-8, 2, -4.5]} args={[2.5, 2.5]} rotation={[0, -Math.PI / 2, 0]} />
                                 <Avatar urlAvatar={"/static/model/avatar/avatar1.glb"} position={[0, 0, 1]} rotation={[0, Math.PI / 3, 0]} />
                                 <Avatar urlAvatar={"/static/model/avatar/avatar2.glb"} position={[0, 0, -1]} rotation={[0, (3 * Math.PI) / 4, 0]} />
                                 <Avatar urlAvatar={"/static/model/avatar/avatar3.glb"} position={[-18, 0, 1]} rotation={[0, Math.PI / 4, 0]} />
